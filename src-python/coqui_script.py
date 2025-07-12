93d0a3f1c7686a6a988ad9704d1451610aef09c4
carlos <carlos@debian1050ti>
2025-07-12 16:13:35 -0300

    12-07-25 16:13

--- src-python/coqui_script.py
+++ src-python/coqui_script.py
@@ -3,7 +3,7 @@ from TTS.api import TTS
 import soundfile as sf
 
 # Texto desde la línea de comandos
-text = sys.argv[1] if len(sys.argv) > 1 else "Este es un texto de prueba."
+text = sys.argv[1] if len(sys.argv) > 1 else "Este es un texto de prueba. Text por defecto."
 
 # Cargar modelo de español
 model_name = "tts_models/es/css10/vits"
